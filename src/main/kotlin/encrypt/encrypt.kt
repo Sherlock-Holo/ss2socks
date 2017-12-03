7f9c2bd944e8d76fea3ac7afacf611cc3d847b40
Sherlock Holo <sherlockya@gmail.com>
2017-12-03 13:59:16 +0800

    Change to CTR mode

--- src/main/kotlin/encrypt/encrypt.kt
+++ src/main/kotlin/encrypt/encrypt.kt
@@ -6,6 +6,9 @@ import javax.crypto.Cipher
 import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
 
+val EncryptMode = 0
+val DecryptMode = 1
+
 private interface GeneralCipher {
     // maybe some encrypt module doesn't use IV or Nonce
     fun getIVorNonce(): ByteArray?
@@ -29,28 +32,42 @@ fun password2key(passwd: String): ByteArray {
     return encodeKey
 }
 
-class AES256CFB(key: ByteArray, private var iv: ByteArray? = null): GeneralCipher {
-    val cipher: Cipher
+class AES256CTR(key: ByteArray, private var iv: ByteArray? = null): GeneralCipher {
+    val cipher = Cipher.getInstance("AES/CTR/NoPadding")
+    val skey = SecretKeySpec(key, "AES")
 
     init {
-        val skey = SecretKeySpec(key, "AES")
-
-        cipher = Cipher.getInstance("AES/CFB/NoPadding")
-
         if (iv != null) {
-            cipher.init(Cipher.DECRYPT_MODE, skey, IvParameterSpec(iv))
+//            cipher.init(Cipher.DECRYPT_MODE, skey, IvParameterSpec(iv))
+            init(DecryptMode, iv)
         } else {
-            cipher.init(Cipher.ENCRYPT_MODE, skey)
-            iv = cipher.iv
+            init(EncryptMode)
+        }
+    }
+
+    private fun init(mode: Int, iv: ByteArray? = null) {
+        when (mode) {
+            EncryptMode -> {
+                cipher.init(Cipher.ENCRYPT_MODE, skey)
+                this.iv = cipher.iv
+            }
+
+            DecryptMode -> {
+                if (iv != null) {
+                    cipher.init(Cipher.DECRYPT_MODE, skey, IvParameterSpec(iv))
+                } else {
+                    cipher.init(Cipher.DECRYPT_MODE, skey, IvParameterSpec(getIVorNonce()))
+                }
+            }
         }
     }
 
     override fun encrypt(plainText: ByteArray): ByteArray {
-        return cipher.doFinal(plainText)
+        return cipher.update(plainText)
     }
 
     override fun decrypt(cipherText: ByteArray): ByteArray {
-        return cipher.doFinal(cipherText)
+        return cipher.update(cipherText)
     }
 
     override fun getIVorNonce(): ByteArray? {
@@ -58,43 +75,28 @@ class AES256CFB(key: ByteArray, private var iv: ByteArray? = null): GeneralCiphe
     }
 
     override fun encrypt(plainBuffer: ByteBuffer, cipherBuffer: ByteBuffer) {
-        cipher.doFinal(plainBuffer, cipherBuffer)
+        cipher.update(plainBuffer, cipherBuffer)
     }
 
     override fun decrypt(cipherBuffer: ByteBuffer, plainBuffer: ByteBuffer) {
-        cipher.doFinal(cipherBuffer, plainBuffer)
+        cipher.update(cipherBuffer, plainBuffer)
+    }
+
+    fun stop() {
+        cipher.doFinal()
     }
 }
 
 fun main(args: Array<String>) {
-    val plainText = "holo".toByteArray()
-    val plainText2 = "sherlock".toByteArray()
-
+    val plainText = "sherlock".toByteArray()
     val key = password2key("qlx")
 
-    val en = AES256CFB(key)
+    val en = AES256CTR(key)
     val iv = en.getIVorNonce()!!
 
-    val de = AES256CFB(key, iv)
+    val de = AES256CTR(key, iv)
+
     val cipherText = en.encrypt(plainText)
-//    val cipherText2 = en.encrypt(plainText2)
-    println("plain text: ${String(plainText)}")
-    val newText = de.decrypt(cipherText)
-//    val newText2 = de.decrypt(cipherText2)
-    println("new text: ${String(newText)}")
-
-    val plainBuffer = ByteBuffer.allocate(100)
-    val cipherBuffer = ByteBuffer.allocate(100)
-    plainBuffer.put(plainText2)
-    plainBuffer.flip()
-    en.encrypt(plainBuffer, cipherBuffer)
-    cipherBuffer.flip()
-    plainBuffer.clear()
-    de.decrypt(cipherBuffer, plainBuffer)
-    plainBuffer.flip()
-    println(String(byteArrayOf(plainBuffer.get())))
-    println(String(byteArrayOf(plainBuffer.get())))
-    println(String(byteArrayOf(plainBuffer.get())))
-    println(String(byteArrayOf(plainBuffer.get())))
-
+    println(String(de.decrypt(cipherText.copyOfRange(0, 3))))
+    println(String(de.decrypt(cipherText.copyOfRange(3, cipherText.size))))
 }
