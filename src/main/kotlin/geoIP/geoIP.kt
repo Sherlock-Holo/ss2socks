1dd4f79d7b864eacc44a29c273e0f9aec6706966
Sherlock-Holo <sherlockya@gmail.com>
2017-12-12 00:16:23 +0800

    Add geoIP

---
+++ src/main/kotlin/geoIP/geoIP.kt
@@ -0,0 +1,42 @@
+package geoIP
+
+import com.maxmind.geoip2.DatabaseReader
+import com.maxmind.geoip2.exception.AddressNotFoundException
+import com.maxmind.geoip2.model.CityResponse
+import java.io.File
+import java.net.InetAddress
+import kotlin.collections.HashMap
+
+
+class GeoIP(filePath: String) {
+    private val dataBaseFile = File(filePath)
+    private val reader: DatabaseReader
+    private val cache = HashMap<String, String>()
+
+    init {
+        if (dataBaseFile.exists()) reader = DatabaseReader.Builder(dataBaseFile).build()
+        else TODO("raise an exception")
+    }
+
+    fun getIPCountry(rawIP: ByteArray): String? {
+        val ip = InetAddress.getByAddress(rawIP).hostAddress
+        if (cache.containsKey(ip)) {
+            return cache[ip]
+        }
+
+        val response: CityResponse
+        try {
+            response = reader.city(InetAddress.getByAddress(rawIP))
+        } catch (e: AddressNotFoundException) {
+            return null
+        }
+        val country = response.country.isoCode
+        cache[ip] = country
+        return country
+    }
+
+    fun isChina(ip: ByteArray): Boolean {
+        if (getIPCountry(ip) == "CN") return true
+        return false
+    }
+}
