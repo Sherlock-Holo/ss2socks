5db65d5a8108ea662887b46f7d07f033b23a8ac4
Sherlock Holo <sherlockya@gmail.com>
2017-12-12 09:14:20 +0800

    Add geoIP config

--- src/main/kotlin/config/config.kt
+++ src/main/kotlin/config/config.kt
@@ -4,24 +4,17 @@ import org.yaml.snakeyaml.Yaml
 import java.io.File
 import java.io.FileInputStream
 
-data class ServerConfig(val ssAddr: String, val ssPort: Int, val backEndAddr: String, val backEndPort: Int, val password: String)
+data class ServerConfig(val ssAddr: String, val ssPort: Int, val backEndAddr: String, val backEndPort: Int,
+                        val password: String, val secretChannel: Boolean, val geoIPDataBaseFilePath: String?)
 
-class Config {
-    private val yamlConfig: Map<String, Any>
-
-    constructor(path: String) {
-        yamlConfig = Yaml().load(FileInputStream(path))
-    }
-
-    constructor(file: File) {
-        yamlConfig = Yaml().load(FileInputStream(file))
-    }
-
-    constructor(fileInputStream: FileInputStream) {
-        yamlConfig = Yaml().load(fileInputStream)
-    }
+class Config(file: File) {
+    private val yamlConfig: Map<String, Any> = Yaml().load(FileInputStream(file))
 
     fun getConfig(): ServerConfig {
-        return ServerConfig(yamlConfig["ssAddr"] as String, yamlConfig["ssPort"] as Int, yamlConfig["backEndAddr"] as String, yamlConfig["backEndPort"] as Int, yamlConfig["password"] as String)
+        if (yamlConfig["geoIP"] as Boolean) {
+            return ServerConfig(yamlConfig["ssAddr"] as String, yamlConfig["ssPort"] as Int, yamlConfig["backEndAddr"] as String, yamlConfig["backEndPort"] as Int, yamlConfig["password"] as String, yamlConfig["geoIP"] as Boolean, yamlConfig["geoIPDatabaseFilePath"] as String)
+        }
+        return ServerConfig(yamlConfig["ssAddr"] as String, yamlConfig["ssPort"] as Int, yamlConfig["backEndAddr"] as String, yamlConfig["backEndPort"] as Int, yamlConfig["password"] as String, yamlConfig["geoIP"] as Boolean, null)
     }
+
 }
