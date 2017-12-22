0c4ad7b9ece35740051895a3af9ed3de6a5452dc
Sherlock Holo <sherlockya@gmail.com>
2017-12-22 11:22:14 +0800

    Add cipher mode config

--- src/main/kotlin/config/config.kt
+++ src/main/kotlin/config/config.kt
@@ -5,7 +5,7 @@ import java.io.File
 import java.io.FileInputStream
 
 data class ServerConfig(val ssAddr: String, val ssPort: Int, val backEndAddr: String, val backEndPort: Int,
-                        val password: String, val secretChannel: Boolean, val geoIPDataBaseFilePath: String?)
+                        val password: String, val cipherMode: String, val secretChannel: Boolean, val geoIPDataBaseFilePath: String?)
 
 class Config(file: File) {
     private val yamlConfig: Map<String, Any> = Yaml().load(FileInputStream(file))
@@ -14,12 +14,12 @@ class Config(file: File) {
         if (yamlConfig["geoIP"] as Boolean) {
             return ServerConfig(yamlConfig["ssAddr"] as String, yamlConfig["ssPort"] as Int,
                     yamlConfig["backEndAddr"] as String, yamlConfig["backEndPort"] as Int,
-                    yamlConfig["password"] as String, yamlConfig["geoIP"] as Boolean, yamlConfig["geoIPDatabaseFilePath"] as String)
+                    yamlConfig["password"] as String, yamlConfig["cipherMode"] as String, yamlConfig["geoIP"] as Boolean, yamlConfig["geoIPDatabaseFilePath"] as String)
         }
 
         return ServerConfig(yamlConfig["ssAddr"] as String, yamlConfig["ssPort"] as Int,
                 yamlConfig["backEndAddr"] as String, yamlConfig["backEndPort"] as Int, yamlConfig["password"] as String,
-                yamlConfig["geoIP"] as Boolean, null)
+                yamlConfig["cipherMode"] as String, yamlConfig["geoIP"] as Boolean, null)
     }
 
 }
