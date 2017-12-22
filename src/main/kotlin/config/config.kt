97e36e3fb40244e2945a7fdf34740155ab80afd7
Sherlock Holo <sherlockya@gmail.com>
2017-12-22 12:48:23 +0800

    Add Toml config

--- src/main/kotlin/config/config.kt
+++ src/main/kotlin/config/config.kt
@@ -1,5 +1,6 @@
 package config
 
+import com.moandjiezana.toml.Toml
 import org.yaml.snakeyaml.Yaml
 import java.io.File
 import java.io.FileInputStream
@@ -22,4 +23,33 @@ class Config(file: File) {
                 yamlConfig["cipherMode"] as String, yamlConfig["geoIP"] as Boolean, null)
     }
 
+}
+
+class Server {
+    lateinit var ssAddr: String
+    var ssPort = 0
+    lateinit var backEndAddr: String
+    var backEndPort = 0
+}
+
+class TopConfig {
+    lateinit var server: Server
+    lateinit var security: Security
+    lateinit var securityChannel: SecurityChannel
+}
+
+class Security {
+    lateinit var cipherMode: String
+    lateinit var password: String
+}
+
+class SecurityChannel {
+    var GeoIP = false
+    lateinit var GeoIPDatabaseFilePath: String
+}
+
+fun main(args: Array<String>) {
+    val tomlconfig = Toml().read(File("/home/sherlock/git/ss2socks/src/main/kotlin/config/config.toml"))
+    val sct = tomlconfig.to(TopConfig::class.java)
+    println(sct.server.ssAddr)
 }
